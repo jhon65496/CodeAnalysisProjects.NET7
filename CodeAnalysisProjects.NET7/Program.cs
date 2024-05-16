74da89092fac2d1edb90a1a4ac3a59f909badf69
jhon65496 <jhon65496@yandex.ru>
2024-05-16 13:45:11 +0300

    Добавьте файлы проекта.

---
+++ CodeAnalysisProjects.NET7/Program.cs
@@ -0,0 +1,22 @@
+﻿using CodeAnalysisProjects.NET7.Services;
+using Microsoft.Build.Evaluation;
+
+namespace CodeAnalysisProjects.NET7
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Test1();
+        }
+
+        
+        static void Test1()
+        {
+            string path = @"e:\Projects\WPF\4587\01_pr\01\github.com\WPFCrudControl-master\fl\GenericCodes.WPF.sln";
+
+            List<Project> table = ProjectAnalysisService.GetProjects(path);            
+        }
+    }
+}
