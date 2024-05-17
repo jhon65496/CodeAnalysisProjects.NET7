beb7aa37d5c99787fab1c92d9bc546e73de05f54
jhon65496 <jhon65496@yandex.ru>
2024-05-17 23:48:42 +0300

    change

---
+++ CodeAnalysisProjectsNET70/Program.cs
@@ -0,0 +1,20 @@
+﻿using CodeAnalysisProjectsNET70.Services;
+using Microsoft.Build.Evaluation;
+
+namespace CodeAnalysisProjectsNET70
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+        }
+
+        static void Test1()
+        {
+            string path = @"e:\Projects\WPF\4587\01_pr\01\github.com\WPFCrudControl-master\fl\GenericCodes.WPF.sln";
+
+            List<Project> table = ProjectAnalysisService.GetProjects(path);
+        }
+    }
+}
---
+++ CodeAnalysisProjectsNET70/Services/ProjectAnalysisService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Build.Construction;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//
+
+
+namespace CodeAnalysisProjectsNET70.Services
+{
+    public static class ProjectAnalysisService
+    {
+
+        public static List<Project> GetProjects(string solutionPath)
+        {
+            // Используйте MSBuildLocator для регистрации версии MSBuild
+            MSBuildLocator.RegisterDefaults();
+            // using var collection = new ProjectCollection();
+            using var workspace = MSBuildWorkspace.Create();
+            var solution = SolutionFile.Parse(solutionPath);
+
+            var solutionProjects = solution.ProjectsInOrder
+                                        .Select(project => workspace.OpenProjectAsync(project.AbsolutePath).Result)
+                                        .Cast<Microsoft.Build.Evaluation.Project>() // Добавьте эту строку
+                                        .ToList();
+                        
+            return solutionProjects;
+        }
+    }
+}
